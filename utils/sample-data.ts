9e20d1c93aaae5fbdd4d0baa75bfb61dab6b3691
HaehunLee <allman956@naver.com>
2023-05-15 23:28:33 +0900

    Init: Next.js with typescript

---
+++ utils/sample-data.ts
@@ -0,0 +1,9 @@
+import { StudyModel } from "../interfaces";
+
+/** Dummy StudyModel data. */
+export const sampleUserData: StudyModel[] = [
+  { id: 101, name: "Alice" },
+  { id: 102, name: "Bob" },
+  { id: 103, name: "Caroline" },
+  { id: 104, name: "Dave" },
+];
