873d12c7021890610fa90106066a6651131f462a
HaehunLee <allman956@naver.com>
2023-05-24 01:57:09 +0900

    Etc: Prettier 설치

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const IndexPage = () => (
   <div>
     <h1>Hello Next.js 👋</h1>
     <p>
-      <Link href="/about">About</Link>
+      <Link href='/about'>About</Link>
     </p>
   </div>
 );
